6a711cb5a73417a618a0bfaecf4d584114118d67
IBA20 <ilya@epir.info>
2022-07-27 23:52:23 +0300

    init

--- main.py
+++ main.py
@@ -1,33 +1,53 @@
 from random import randint
 
 
-def choose_elements(lst, k):
-    """Ответ на вопрос 1. Сложность алгоритма O(k). Случай k —> N проблем не вызывает."""
-    input_list = lst[:]  # создаем копию, чтобы не портить исходный список
-    output_list = []
-    n = len(input_list) - 1
-    for i in range(k):
-        output_list.append(input_list.pop(randint(0, n - i)))
+def choose_elements(lst: list, k: int) -> list:
+    """Ответ на вопрос 1"""
+    n = len(lst)
+    chosen = set()
+    if k <= n // 2:
+        # собираем индексы, которые войдут в итоговый список:
+        while len(chosen) < k:
+            chosen.add(randint(0, n - 1))
+
+        output_list = [lst[i] for i in chosen]
+    else:
+        # собираем индексы, которые не войдут в итоговый список:
+        while len(chosen) < n - k:
+            chosen.add(randint(0, n - 1))
+
+        output_list = [lst[i] for i in range(n) if i not in chosen]
+
+        # перемешиваем полученный список:
+        for i in range(k - 1):
+            j = randint(i + 1, k - 1)
+            output_list[i], output_list[j] = output_list[j], output_list[i]
     return output_list
 
 
-def choose_elements_prob(lst, prob, k):
-    """Ответ на вопрос 2. Сложность алгоритма O(k). Случай k —> N проблем не вызывает."""
-    input_list = lst[:]  # создаем копию, чтобы не портить исходный список
-    prob_list = prob[:]  # создаем копию, чтобы не портить исходный список
-    output_list = []
-    n = len(input_list) - 1
-    while k > 0:
-        index = randint(0, n)
-        if randint(0, 10) <= 10 * prob_list[index]:  # применяем выроятность из списка prob к выбранному индексу
-            output_list.append(input_list.pop(index))
-            prob_list.pop(index)
-            k -= 1
-            n -= 1
+def choose_elements_prob(lst: list, prob: list, k: int) -> list:
+    """Ответ на вопрос 2"""
+    n = len(lst)
+    chosen = set()
+    if k <= n // 2:
+        # собираем индексы, которые войдут в итоговый список:
+        while len(chosen) < k:
+            ind = randint(0, n - 1)
+            if prob[ind] >= randint(1, 10) / 10:
+                chosen.add(ind)
+
+        output_list = [lst[i] for i in chosen]
+    else:
+        # собираем индексы, которые не войдут в итоговый список:
+        while len(chosen) < n - k:
+            ind = randint(0, n - 1)
+            if prob[ind] <= randint(1, 10) / 10:
+                chosen.add(ind)
+
+        output_list = [lst[i] for i in range(n) if i not in chosen]
+
+        # перемешиваем полученный список:
+        for i in range(k - 1):
+            j = randint(i + 1, k - 1)
+            output_list[i], output_list[j] = output_list[j], output_list[i]
     return output_list
-
-
-if __name__ == '__main__':
-    input_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    prob = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
-    print(choose_elements_prob(input_list, prob, 9))
